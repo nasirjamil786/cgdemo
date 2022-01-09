a019117862b69cbd5ee003f82ee458bd8caf6205
Nasir Jamil <nasir@nasirjamil.com>
2022-01-09 15:58:00 +0000

    embedData second argument should be .png

--- resources/views/order/email.blade.php
+++ resources/views/order/email.blade.php
@@ -12,7 +12,7 @@
 @section('signature')
 
     @if($order->signature != NULL)
-        <img alt="sign" src="<?php echo $message->embedData($order->signature,'sig');  ?>" width="220" height="80" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 12px;" border="0"> 
+        <img alt="sign" src="<?php echo $message->embedData($order->signature,'sign.png');  ?>" width="220" height="80" style="display: block; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 12px;" border="0"> 
     @endif
 
 
